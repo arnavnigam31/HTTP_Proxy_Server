7127808acd6904a4383d4fa2b98196df8f5d9636
arnavnigam31 <143436271+arnavnigam31@users.noreply.github.com>
2026-04-02 15:08:23 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ echo "Compiling C proxy..."
 gcc Proxy_server_cache.c proxy_parse.c -o proxy -lpthread
 
 echo "Starting proxy..."
-./proxy_server 8080 &
+./proxy 8080 &
 
 echo "Starting node bridge..."
 node server.js 
