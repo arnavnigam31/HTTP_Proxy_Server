faa5232a380e250c3fceecc62b2c1627cc768f13
arnavnigam31 <143436271+arnavnigam31@users.noreply.github.com>
2026-04-02 14:01:19 +0530

    Add start script for compiling and running proxy

---
+++ start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Compiling C proxy..."
+gcc Proxy_server_cache.c proxy_parse.c -o proxy -lpthread
+
+echo "Starting proxy..."
+./proxy 8080 &
+
+echo "Starting node bridge..."
+node server.js 8080
